9a2ae0ca2362a6cf108c16d095f4d8a3926a82fc
elsheikh <ahmedhelsheikh@gmail.com>
2024-09-26 19:11:17 +0200

    fix: :bug: Fix Netlify Build | Unused imports
    Removed unused import of useEffect and axios

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ReactPaginate from "react-paginate";
 import "./ProjectsSection.css";
@@ -174,7 +173,10 @@ const ProjectsSection = () => {
 
   /* TODO 
   Projects Fetching
-  Uncomment this section when connecting backend
+
+  import axios from "axios";
+  import React, { useState, useEffect } from "react";
+
   // Fetch projects from the backend API
   useEffect(() => {
     axios
